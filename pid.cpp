8a26ffa65810ecb20c4a7700ae5f3357d89def23
potatehoes <acetousk@users.noreply.github.com>
2019-06-27 01:03:11 -0700

    Create pid.cpp

---
+++ pid.cpp
@@ -0,0 +1,58 @@
+#include "pid.hpp"
+
+Pid::Pid( double kPset ){
+    kP = kPset;
+    kI = 0;
+    kD = 0;
+}
+
+Pid::Pid( double kPset, double kDset ){
+    kP = kPset;
+    kI = 0;
+    kD = kDset;
+}
+
+Pid::Pid( double kPset, double kIset, double kDset ){
+    kP = kPset;
+    kI = kIset;
+    kD = kDset;    
+}
+
+void Pid::eSetRange( int eRangeIn ){
+    eRange = eRangeIn;
+}
+
+void Pid::setMaxI( int maxIIn ){
+    maxI = maxIIn;
+}
+
+double Pid::calculate( double error ){
+    p = error;
+    //all for proportional
+
+    i += error; //area under curve      //TODO: see pdf 3.1
+    if( std::abs(error) <= eRange ){
+        error = 0; //pid loops can continue forever... until now
+    }
+    if( std::abs(i) > maxI ){
+        i = maxI;       //if i's positive
+        if( i < 0 ){
+            i *= -1;    //if i's negative
+        }
+    }
+    //all for integral
+
+    d = error - eLast;
+    eLast = error;
+    //all for derivative
+
+    pros::delay(dT);
+    //gotta delay
+
+    return((             
+        ( kP * p ) +    /*  p   p  */
+        ( kI * i ) +    /*    i    */
+        ( kD * d )      /*  \ddd/  */
+    ));
+    //done (as if you didnt already know)
+}
